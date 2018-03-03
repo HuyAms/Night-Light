e4d9a02f5287b8179e95a5a6f67cef92ba5426ee
ZeusLMT <lemanhtuan275@gmail.com>
2018-03-03 17:33:40 +0200

    working on comments page

---
+++ src/model/comment.ts
@@ -0,0 +1,6 @@
+export class Comment {
+  constructor(public userID: string,
+              public commentID: string,
+              public timestamp: string,
+              public content: string){}
+}
