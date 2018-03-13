bb41c55cad48ffac9a2bd533fadc1da135143237
Tran Cong Thanh <congthanhptnk@Trans-MacBook-Pro.local>
2018-03-13 10:40:05 +0200

    fix link

--- src/pages/aboutus/aboutus.ts
+++ src/pages/aboutus/aboutus.ts
@@ -19,19 +19,19 @@ export class AboutusPage {
 
   teams = [
     {
-      imageUrl: '../../assets/imgs/HuyTrinh.jpg',
+      imageUrl: 'assets/imgs/HuyTrinh.jpg',
       quote: 'Dreams don\'t work unless you do.',
       name: 'Huy Trinh',
       position: 'Software Developer'
     },
     {
-      imageUrl: '../../assets/imgs/TuanLe.jpg',
+      imageUrl: 'assets/imgs/TuanLe.jpg',
       quote: 'Everything happens for a reason.',
       name: 'Tuan Le',
       position: 'Software Developer'
     },
     {
-      imageUrl: '../../assets/imgs/ThanhTran.jpg',
+      imageUrl: 'assets/imgs/ThanhTran.jpg',
       quote: 'People didn\'t normally hate bugs. Then came IT.',
       name: 'Thanh Tran',
       position: 'Software Developer'
